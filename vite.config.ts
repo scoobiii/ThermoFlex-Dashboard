4702898223b6e6810926a22dad1205d6da3a96c8
scoobiii <sobrinhosj@gmail.com>
2025-09-22 16:01:00 +0000

    atualizado vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/ThermoFlex-Dashboard/',
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+          // Use variáveis prefixadas com VITE_ para o frontend
+          'process.env.VITE_GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)
       },
       resolve: {
         alias: {
